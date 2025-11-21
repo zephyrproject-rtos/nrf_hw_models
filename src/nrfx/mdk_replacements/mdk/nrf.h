9022ee285de48347dd1d803d655444b0f29398b4
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2025-11-21 15:08:12 +0100

    NRF: add option to skip including mocked MDK symbols
    Some software components may depent on hardware properties
    described by MDK. Allow to access them rather than mocked
    counterparts in such cases.
    
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>

--- src/nrfx/mdk_replacements/mdk/nrf.h
+++ src/nrfx/mdk_replacements/mdk/nrf.h
@@ -15,7 +15,12 @@
 /* Include the nrf from stable MDK. */
 #include <bsp/stable/mdk/nrf.h>
 
-/* For BSIM we require redefining the pointers to the peripherals. */
+#if !defined(NRF_H_NO_BSIM_REDEFS)
+/* For BSIM peripherals registers are not mapped in the same addresses as in
+ * the real HW so we need to correct them.
+ * For the very special cases in which we want the real HW addresses after
+ * including nrf.h, define NRF_H_NO_BSIM_REDEFS before include *any* header */
 #include "nrf_bsim_redef.h"
+#endif
 
 #endif
